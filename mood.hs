adf67ffdb90714cdf75719d23627ad48ff97d1c9
william <4yellowpeppers@gmail.com>
2017-08-09 23:19:05 -0400

    changed derived Eq/Show to manual instances

--- mood.hs
+++ mood.hs
@@ -1,8 +1,19 @@
 data Mood = Blah | Woot
-    deriving (Eq, Show)
+    --deriving (Eq, Show)
 
---changeMood :: Mood -> Mood
-changeMood :: (->) Mood Mood
+instance Eq Mood
+    where
+        (==) Blah Blah  = True
+        (==) Woot Woot  = True
+        (==) _ _        = False
+
+instance Show Mood
+    where
+        show Blah = "Blah"
+        show Woot = "Woot"
+
+--changeMood :: Mood -> Mood    -- These are identical
+changeMood :: (->) Mood Mood    -- type declarations
 changeMood m
     | m == Blah = Woot
     | otherwise = Blah
