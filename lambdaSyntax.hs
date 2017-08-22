02077f92d9a29e19a57c3dc328887a41684a6220
william <wersing@invidi.com>
2017-08-22 15:13:59 -0400

    completed 'Grab Bag' section from ch 7.3

--- lambdaSyntax.hs
+++ lambdaSyntax.hs
@@ -1,8 +1,21 @@
--- Practice writing anonymous lambda syntax (ch 07)
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+-- 3. Practice writing anonymous lambda syntax (ch 07)
 -- 
--- a) Rewrite the f function in the where clause
+--  a) Rewrite the f function in the where clause.
 addOneIfOdd n = case odd n of
-    True    -> f
+    True    -> f n
     False   -> n
-    --where f n = n + 1
-    where f = n + 1
+    --where f n = n + 1     -- original
+    where f = \n -> n + 1   -- using lambda
+
+
+--  b) Rewrite the following to use anonymous lambda syntax:
+--addFive x y = (if x > y then y else x) + 5        -- original
+addFive = (\x y -> (if x > y then y else x) + 5)    -- using lambda
+--addFive = (\x y -> (min x y) + 5)                 -- alternative lambda
+
+
+--  c) Rewrite the following so that it doesn't use anonymous lambda syntax
+--mflip f = \x -> \y -> f y x   -- original
+mflip f x y = f y x             -- w/o lambda
