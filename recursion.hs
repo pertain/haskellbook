a199e22e31db889e17f9ecc0b1afc9cedd56d27d
william <4yellowpeppers@gmail.com>
2017-09-01 00:21:38 -0400

    added mc91 -- cleaned up comments

--- recursion.hs
+++ recursion.hs
@@ -4,14 +4,14 @@
  -- Naive and incomplete
  -- (doesn't handle denominator <= 0)
 divRec :: Integral a => a -> a -> a
-divRec n d = go n d 0
+divRec numer denom = go numer denom 0
     where
         go n d q
             | n < d     = q
             | otherwise = go (n - d) d (q + 1)
 -}
 
--- Wraps output in Maybe, and handles denom of 0
+-- Wraps output in Maybe to deal with denom = 0
 -- (need to add support for denom < 0)
 divRec :: Integral a => a -> a -> Maybe a
 divRec numer denom = go numer denom (Just 0)
@@ -20,3 +20,8 @@ divRec numer denom = go numer denom (Just 0)
             | d == 0    = Nothing
             | n < d     = Just q
             | otherwise = go (n - d) d (Just (q + 1))
+
+mc91 :: Integral a => a -> a
+mc91 n
+    | n > 100   = n - 10
+    | otherwise = mc91 (mc91 (n + 11))
