c76f8a167676c1947fb739da2b9a6105c6e69596
william <4yellowpeppers@gmail.com>
2017-09-02 22:32:32 -0400

    initial commit -- recreate words and lines functions (ch 09)

---
+++ poemLines.hs
@@ -0,0 +1,51 @@
+-- Thy Fearful Symmetry (ch 09)
+
+module PoemLines where
+
+hns         = "holas nolas scrolas"
+
+firstSen    = "Tyger Tyger, burning bright\n"
+secondSen   = "In the forests of the night\n"
+thirdSen    = "What immortal hand or eye\n"
+fourthSen   = "Could frame thy fearful symmetry?"
+
+sentences = firstSen ++ secondSen ++
+            thirdSen ++ fourthSen
+
+-- splits on whitespace
+myWords :: String -> [String]
+myWords []  = []
+myWords l   = x : myWords xs
+    where
+        x   = takeWhile (/= ' ') l
+        xs  = (dropWhile (== ' ') . dropWhile (/= ' ')) l
+
+-- splits on newline
+myLines :: String -> [String]
+--myLines = (???)
+myLines []  = []
+myLines l   = x : myLines xs
+    where
+        x   = takeWhile (/= '\n') l
+        xs  = (dropWhile (== '\n') . dropWhile (/= '\n')) l
+
+-- parameterizes the split character
+breakOn :: String -> Char -> [String]
+breakOn [] _    = []
+breakOn l c     = x : breakOn xs c
+    where
+        x   = takeWhile (/= c) l
+        xs  = (dropWhile (== c) . dropWhile (/= c)) l
+
+shouldEqual =
+    [ "Tyger Tyger, burning bright"
+    , "In the forests of the night"
+    , "What immortal hand or eye"
+    , "Could frame thy fearful symmetry?"
+    ]
+
+main :: IO ()
+main = print $
+       "Are they equal? "
+       ++ show (breakOn sentences '\n'
+                == shouldEqual)
