568df5d4aa0e976a320803b0e594e7d6fb885969
william <4yellowpeppers@gmail.com>
2017-09-16 15:10:01 -0400

    exercise completed

--- db.hs
+++ db.hs
@@ -20,4 +20,33 @@ theDatabase =
 -- 1) Write a function that filters for DbDate values
 --    and returns a list of UTCTime values inside them.
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate = undefined
+--filterDbDate = undefined
+filterDbDate []                 = []
+filterDbDate ((DbDate (x)):xs)  = x : filterDbDate xs
+filterDbDate (_:xs)             = filterDbDate xs
+
+-- 2) Write a function that filters for DbNumber values
+--    and returns a list of the Integer values inside them.
+filterDbNumber :: [DatabaseItem] -> [Integer]
+--filterDbNumber = undefined
+filterDbNumber []                   = []
+filterDbNumber ((DbNumber (x)):xs)  = x : filterDbNumber xs
+filterDbNumber (_:xs)               = filterDbNumber xs
+
+-- 3) Write a function that gets the most recent date
+mostRecent :: [DatabaseItem] -> UTCTime
+--mostRecent = undefined
+mostRecent = maximum . filterDbDate
+
+-- 4) Write a function that sums all of the DbNumber values
+sumDb :: [DatabaseItem] -> Integer
+--sumDb = undefined
+sumDb = sum . filterDbNumber
+
+-- 5) Write a function that gets the average of the DbNumber values
+avgDb :: [DatabaseItem] -> Double
+--avgDb = undefined
+avgDb xs = total / count
+    where
+        count = (fromIntegral . length) (filterDbNumber xs)
+        total = fromIntegral (sumDb xs)
