7c587848d3c07f038ab7503d01906a84cea370b6
william <wersing@invidi.com>
2017-10-17 16:49:56 -0400

    Added mapTree and traversal functions

--- binaryTree.hs
+++ binaryTree.hs
@@ -9,3 +9,33 @@ insert' b (Node l a r)
     | b == a = Node l a r
     | b < a  = Node (insert' b l) a r
     | b > a  = Node l a (insert' b r)
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node l a r) = Node (mapTree f l) (f a) (mapTree f r)
+
+testTree' :: BinaryTree Integer
+testTree' = Node (Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)) 1 (Node Leaf 3 Leaf)
+
+mapExpected :: BinaryTree Integer
+mapExpected = Node (Node (Node Leaf 5 Leaf) 3 (Node Leaf 6 Leaf)) 2 (Node Leaf 4 Leaf)
+
+mapOkay =
+    if mapTree (+1) testTree' == mapExpected
+    then print "Correct"
+    else error "Not Correct"
+
+-- preorder (root left right)
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node l a r) = a : (preorder l ++ preorder r)
+
+-- inorder (left root right)
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node l a r) = inorder l ++ (a : inorder r)
+
+-- postorder (left right root)
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node l a r) = postorder l ++ postorder r ++ [a]
