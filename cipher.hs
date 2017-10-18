1eb07556e516fd22e66201a0cc815b7d85008f06
william <4yellowpeppers@gmail.com>
2017-10-18 16:48:40 -0400

    Modified caesarCipher to use where

--- cipher.hs
+++ cipher.hs
@@ -6,12 +6,17 @@ import Data.Char
 
 caesarCipher :: Int -> Char -> Char
 caesarCipher n c
-    | generalCategory c == LowercaseLetter  = chr (ord 'a' + (mod ((ord c - ord 'a') + n) 26))
-    | generalCategory c == UppercaseLetter  = chr (ord 'A' + (mod ((ord c - ord 'A') + n) 26))
-    | otherwise                             = ' '
+    | isLC      = chr (ord 'a' + (mod ((ord c - ord 'a') + n) 26))
+    | isUC      = chr (ord 'A' + (mod ((ord c - ord 'A') + n) 26))
+    | otherwise = ' '
+    where
+        isLC = generalCategory c == LowercaseLetter
+        isUC = generalCategory c == UppercaseLetter
 
-encipher :: Int -> String -> String
-encipher n = map (caesarCipher n)
+cEncipher :: Int -> String -> String
+cEncipher n = map (caesarCipher n)
 
-decipher :: Int -> String -> String
-decipher n = map (caesarCipher (negate n))
+cDecipher :: Int -> String -> String
+cDecipher n = map (caesarCipher (negate n))
+
+--vigenereCipher :: 
