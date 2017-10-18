cdb0cdd9fd1017d5bbedd266544b94530879386f
william <4yellowpeppers@gmail.com>
2017-10-17 22:32:31 -0400

    added traversal tests -- added foldr implementation for binaryTree

--- binaryTree.hs
+++ binaryTree.hs
@@ -14,16 +14,26 @@ mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
 mapTree _ Leaf = Leaf
 mapTree f (Node l a r) = Node (mapTree f l) (f a) (mapTree f r)
 
-testTree' :: BinaryTree Integer
-testTree' = Node (Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)) 1 (Node Leaf 3 Leaf)
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
 
-mapExpected :: BinaryTree Integer
-mapExpected = Node (Node (Node Leaf 5 Leaf) 3 (Node Leaf 6 Leaf)) 2 (Node Leaf 4 Leaf)
+testTree1 :: BinaryTree Integer
+testTree1 = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
 
-mapOkay =
-    if mapTree (+1) testTree' == mapExpected
-    then print "Correct"
-    else error "Not Correct"
+testTree2 :: BinaryTree Integer
+testTree2 = Node (Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)) 1 (Node Leaf 3 Leaf)
+
+mapExpected1 :: BinaryTree Integer
+mapExpected1 = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+mapExpected2 :: BinaryTree Integer
+mapExpected2 = Node (Node (Node Leaf 5 Leaf) 3 (Node Leaf 6 Leaf)) 2 (Node Leaf 4 Leaf)
+
+testMap :: IO ()
+testMap =
+    if mapTree (+1) testTree1 == mapExpected1
+    then putStrLn "mapTree passed"
+    else putStrLn "mapTree failed"
 
 -- preorder (root left right)
 preorder :: BinaryTree a -> [a]
@@ -39,3 +49,36 @@ inorder (Node l a r) = inorder l ++ (a : inorder r)
 postorder :: BinaryTree a -> [a]
 postorder Leaf = []
 postorder (Node l a r) = postorder l ++ postorder r ++ [a]
+
+testPreorder :: IO ()
+testPreorder 
+    | prePassed = putStrLn "Preorder passed"
+    | otherwise = putStrLn "Preorder failed"
+    where
+        prePassed = preorder testTree == [2, 1, 3]
+
+testInorder :: IO ()
+testInorder
+    | inPassed  = putStrLn "Inorder passed"
+    | otherwise = putStrLn "Inorder failed"
+    where
+        inPassed = inorder testTree == [1, 2, 3]
+
+testPostorder :: IO ()
+testPostorder
+    | postPassed = putStrLn "Postorder passed"
+    | otherwise  = putStrLn "Postorder failed"
+    where
+        postPassed = postorder testTree == [1, 3, 2]
+
+-- preorder implementation of foldr for binary tree
+foldrTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldrTree _ z Leaf = z
+foldrTree f z (Node l a r) = foldrTree f (foldrTree f (f a z) l) r
+
+main :: IO ()
+main = do
+    testMap
+    testPreorder
+    testInorder
+    testPostorder
