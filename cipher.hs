e010608247821104c58179521d62f174414b4a11
william <4yellowpeppers@gmail.com>
2017-10-18 23:20:29 -0400

    Added Vigenere cipher -- may still want to add support for both uppercase and lowercase (like Caesar cipher)

--- cipher.hs
+++ cipher.hs
@@ -1,15 +1,18 @@
--- Caesar Cipher (ch. 09)
+-- Caesar Cipher (ch. 09), Vigenere Cipher (ch. 11)
 
 module Cipher where
 
 import Data.Char
+import Data.List.Split (splitPlaces)
 
+-- Caesar Cipher
 caesarCipher :: Int -> Char -> Char
 caesarCipher n c
-    | isLC      = chr (ord 'a' + (mod ((ord c - ord 'a') + n) 26))
-    | isUC      = chr (ord 'A' + (mod ((ord c - ord 'A') + n) 26))
+    | isLC      = chr (ord 'a' + (mod (alphIndex + n) 26))
+    | isUC      = chr (ord 'A' + (mod (alphIndex + n) 26))
     | otherwise = ' '
     where
+        alphIndex = ord (toLower c) - ord 'a'
         isLC = generalCategory c == LowercaseLetter
         isUC = generalCategory c == UppercaseLetter
 
@@ -19,4 +22,38 @@ cEncipher n = map (caesarCipher n)
 cDecipher :: Int -> String -> String
 cDecipher n = map (caesarCipher (negate n))
 
---vigenereCipher :: 
+
+-- Viginere Cipher
+vigenereCipher :: (Int, Char) -> Char
+vigenereCipher (k, c)
+    | vValidChar c = chr (ord 'a' + (mod (alphIndex + k) 26))
+    | otherwise = ' '
+    where
+        alphIndex = ord (toLower c) - ord 'a'
+
+vCipherKey :: String -> String -> String
+vCipherKey s k = unwords $ splitPlaces ls ks
+    where
+        ss = words s
+        ks = take (length s) $ concat $ repeat k
+        ls = map length ss
+
+vValidChar :: Char -> Bool
+vValidChar c = generalCategory (toLower c) == LowercaseLetter
+
+vShiftVal :: Char -> Int
+vShiftVal k = ord (toLower k) - ord 'a'
+
+vEncipher :: String -> String -> String
+vEncipher s k = map vigenereCipher pairs
+    where
+        key = vCipherKey s k
+        shifts = map vShiftVal key
+        pairs = zip shifts s
+
+vDecipher :: String -> String -> String
+vDecipher s k = map vigenereCipher pairs
+    where
+        key = vCipherKey s k
+        shifts = map (negate . vShiftVal) key
+        pairs = zip shifts s
