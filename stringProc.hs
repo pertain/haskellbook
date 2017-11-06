4b1bf96af04d29b56e83f5813f6c0b4866051b41
william <4yellowpeppers@gmail.com>
2017-11-05 20:44:22 -0500

    Initial commit -- String Processing exercises (ch 12)

---
+++ stringProc.hs
@@ -0,0 +1,39 @@
+-- String Processing exercises -- end of chapter (ch 12)
+
+import Data.Char (toLower)
+
+-- 1) Write a recursive function named replaceThe which takes a text/string,
+--    breaks it into words, and replaces each instance of "the" with "a".
+--    It's intended to replace only the word "the".
+replaceThe :: String -> String
+replaceThe = unwords . swap . words
+    where
+        swap []         = []
+        swap ("the":xs) = "a" : swap xs
+        swap (x:xs)     = x : swap xs
+
+
+-- Helper function for countTheBeforeVowel
+beginsWithVowel :: String -> Integer
+beginsWithVowel []    = 0
+beginsWithVowel (x:_) = case elem (toLower x) "aeiouy" of
+    True -> 1
+    _    -> 0
+
+-- 2) Write a recursive function that takes a text/string,
+--    breaks it into words, and counts the number of instances
+--    of "the" followed by a vowel-initial word.
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel = next . words
+    where
+        next []           = 0
+        next ("the":x:xs) = beginsWithVowel x + next xs
+        next (_:xs)       = next xs
+
+
+-- 3) Return the number of vowels in a word.
+countVowels :: String -> Integer
+countVowels [] = 0
+countVowels (x:xs) = case elem x "aeiouy" of
+    True -> 1 + countVowels xs
+    _    -> countVowels xs
