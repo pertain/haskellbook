a0ebcc3e3fdd2437f91de22d56f861a75f038235
william <4yellowpeppers@gmail.com>
2017-11-17 21:48:18 -0500

    Added myUnfoldr and betterIterate functions

--- unfolding.hs
+++ unfolding.hs
@@ -7,3 +7,17 @@ myIterate f a = go f [] a
     where
         go :: (a -> a) -> [a] -> a -> [a]
         go f' xs x = x : go f' (f' x : xs) (f' x)
+
+-- 2) Write the function myUnfoldr using direct recursion.
+--    The output should match that of 'unfoldr'.
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f b = go f [] b
+    where
+        go :: (b -> Maybe (a, b)) -> [a] -> b -> [a]
+        go f' as b' = case f' b' of
+            Nothing       -> []
+            Just (a, b'') -> a : go f' (a : as) b''
+
+-- 3) Rewrite myIterate into betterIterate using myUnfoldr.
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f a = myUnfoldr (\a -> Just (a, f a)) a
