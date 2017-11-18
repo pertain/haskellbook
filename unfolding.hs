9fa6a978351e1afa0914d7ed3cac0bb41660af4b
william <4yellowpeppers@gmail.com>
2017-11-18 15:57:08 -0500

    Added unfold for BinaryTree

--- unfolding.hs
+++ unfolding.hs
@@ -3,21 +3,45 @@
 -- 1) Write the function myIterate using direct recursion.
 --    The output should match that of 'iterate'.
 myIterate :: (a -> a) -> a -> [a]
-myIterate f a = go f [] a
+myIterate f x = go f [] x
     where
         go :: (a -> a) -> [a] -> a -> [a]
-        go f' xs x = x : go f' (f' x : xs) (f' x)
+        go f' xs x' = x' : go f' (f' x' : xs) (f' x')
 
 -- 2) Write the function myUnfoldr using direct recursion.
 --    The output should match that of 'unfoldr'.
 myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-myUnfoldr f b = go f [] b
+myUnfoldr f x = go f [] x
     where
         go :: (b -> Maybe (a, b)) -> [a] -> b -> [a]
-        go f' as b' = case f' b' of
+        go f' ys x' = case f' x' of
             Nothing       -> []
-            Just (a, b'') -> a : go f' (a : as) b''
+            Just (y, x'') -> y : go f' (y : ys) x''
 
 -- 3) Rewrite myIterate into betterIterate using myUnfoldr.
 betterIterate :: (a -> a) -> a -> [a]
-betterIterate f a = myUnfoldr (\a -> Just (a, f a)) a
+betterIterate f x = myUnfoldr (\x' -> Just (x', f x')) x
+
+
+-- Unfold for Binary Tree
+
+data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a)
+    deriving (Eq, Show)
+
+-- 1) Write unfold for BinaryTree
+unfoldBT :: (a -> Maybe (a, b, a)) -> a -> BinaryTree b
+unfoldBT f x = go f Leaf x
+    where
+        go :: (a -> Maybe (a, b, a)) -> BinaryTree b -> a -> BinaryTree b
+        go f' bt x' = case f' x' of
+            Nothing          -> Leaf
+            Just (xl, y, xr) -> Node (go f' bt xl) y (go f' bt xr)
+
+-- 2) Write treeBuild using unfoldBT
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n = unfoldBT go 0
+    where
+        go :: Integer -> Maybe (Integer, Integer, Integer)
+        go i = case i < n of
+            True  -> Just (i + 1, i, i + 1)
+            False -> Nothing
