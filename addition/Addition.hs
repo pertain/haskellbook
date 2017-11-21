2bc288b9fca1b2595ebd25d176cd22d079edf69a
william <4yellowpeppers@gmail.com>
2017-11-21 18:08:43 -0500

    Added several QuickCheck Gen functions

--- addition/Addition.hs
+++ addition/Addition.hs
@@ -24,7 +24,7 @@ main = hspec $ do
             sumBy 8 9 `shouldBe` 72
     describe "Properties" $ do
         it "x + 1 is always greater than x" $ do
-            property $ \x -> x + 1 > (x :: Int)
+            property $ \x -> x + 1 > (x :: Int) -- using QuickCheck inside hspec
 
 sayHello :: IO ()
 sayHello = putStrLn "hello!"
@@ -42,3 +42,85 @@ sumBy num1 num2 = go num1 num2 0
         go x y total
             | y == 0    = total
             | otherwise = go x (y - 1) (total + x)
+
+
+-- Playing with Gen (QuickCheck)
+
+-- Only a single value is possible
+trivialInt :: Gen Int
+trivialInt = return 1
+
+-- Each of 1, 2, and 3 have the same probability of being picked
+oneThroughThree :: Gen Int
+oneThroughThree = elements [1, 2, 3]
+
+-- Now 2 has a higher probability of being picked
+oneThroughThree' :: Gen Int
+oneThroughThree' = elements [1, 2, 2, 2, 2, 3]
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+--
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [Left a, Right b]
+
+-- equal probability
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+    a <- arbitrary
+    elements [Nothing, Just a]
+
+-- This is what QuickCheck does so
+-- you get more Just values
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+    a <- arbitrary
+    frequency [ (1, return Nothing)
+              , (3, return (Just a))]
+
+
+-- Use QuickCheck without hspec
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+prop_subtractionLessThan :: Int -> Bool
+prop_subtractionLessThan x = x - 1 < x
+
+-- This will fail (intentionally)
+prop_additionLessThan :: Int -> Bool
+prop_additionLessThan x = x - 1 > x
+
+-- runQc is like main for QuickCheck
+runQc :: IO ()
+runQc = do
+    quickCheck prop_additionGreater
+    quickCheck prop_subtractionLessThan
+    quickCheck prop_additionLessThan
