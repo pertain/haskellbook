991eb2bffcec18a3f2b92fcafc7ba8ab8e0a8a10
william <4yellowpeppers@gmail.com>
2017-11-23 00:13:05 -0500

    Added associativity and commutativity tests for addition

--- qCheck.hs
+++ qCheck.hs
@@ -33,7 +33,24 @@ prop_sortedIsSorted :: [Int] -> Bool
 prop_sortedIsSorted xs = listOrdered (sort xs) == True
 
 
+-- 3) These properties will not hold for floating point numbers
+-- Test Associative and Commutative properties of addition
+plusAssociative :: (Eq a, Num a) => a -> a -> a -> Bool
+plusAssociative x y z = x + (y + z) == (x + y) + z
+
+prop_associativeAddition :: Int -> Int -> Int -> Bool
+prop_associativeAddition x y z = plusAssociative x y z == True
+
+plusCommutative :: (Eq a, Num a) => a -> a -> Bool
+plusCommutative x y = x + y == y + x
+
+prop_commutativeAddition :: Int -> Int -> Bool
+prop_commutativeAddition x y = plusCommutative x y == True
+
+
 runQc :: IO ()
 runQc = do
     quickCheck prop_doubleThenHalve
     quickCheck prop_sortedIsSorted
+    quickCheck prop_associativeAddition
+    quickCheck prop_commutativeAddition
