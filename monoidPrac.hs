b7ebcd15caca538f9e3d1f47f647c02794bc61b0
william <4yellowpeppers@gmail.com>
2017-11-27 23:03:53 -0500

    Initial commit -- monoid practice exercises (ch 15)

---
+++ monoidPrac.hs
@@ -0,0 +1,14 @@
+-- Monoid exercises (ch 15)
+
+
+-- Optional Monoid
+
+data Optional a = Nada | Only a
+    deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a)
+    where
+        mempty = Nada
+        mappend Nada x = x
+        mappend x Nada = x
+        mappend (Only x) (Only y) = Only (mappend x y)
