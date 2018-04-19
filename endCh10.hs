f22bab42bbd6b3d010a2c6eb1cc5cedca79e22d8
william <4yellowpeppers@gmail.com>
2018-04-19 08:10:08 -0400

    Initial commit -- Completed the first section (Vowels and Consonants)

---
+++ endCh10.hs
@@ -0,0 +1,32 @@
+-- endCh10.hs
+--
+-- End of chapter exercises (ch 10)
+
+
+-- Vowels and Consonants
+--
+-- Given the following sets of consonants and vowels:
+stops = "pbtdkg"
+vowels = "aeiou"
+
+-- a)
+-- Write a function that produces 3-tuples for
+-- all possible stop-vowel-stop combinations
+svs :: [(Char,Char,Char)]
+svs = [(s,v,s') | s <- stops, v <- vowels, s' <- stops]
+
+-- b)
+-- Modify svs so that it only returns
+-- combinations that begin with 'p'
+psvs :: [(Char,Char,Char)]
+psvs = [(s,v,s') | s <- stops, v <- vowels, s' <- stops, s == 'p']
+
+-- c)
+-- Set up lists of nouns and verbs, and write a
+-- function that produces 3-tuples representing
+-- possible noun-verb-noun sentences
+noun = ["program", "beet", "keycap", "headphones", "coffee", "record"]
+verb = ["listen", "obtain", "consider", "lean", "loafe"]
+
+nvn :: [(String,String,String)]
+nvn = [(n,v,n') | n <- noun, v <- verb, n' <- noun]
